8ef61181f24426199ce0383a63a2375fcc89890a
maulanabin <2041720132@student.polinema.ac.id>
2022-07-22 13:12:41 +0700

    Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lana Blog | About</title>
-    <link rel="stylesheet" href="css/style.css">
+@extends('layouts.main')
 
-</head>
-<body>
+
+@section('container')
     <h1>Halaman About</h1>
-    <h3><?=$name; ?></h3>
-    <p><?=$email; ?></p>
-    <img src=<?=$image; ?> alt=<?=$name; ?> width="200px">
-</body>
-</html>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}"" width="200px">
+@endsection
